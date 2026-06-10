bec971daffabc5468ee1e99bf85900ed9ed40271
Gaby Nieva <gaby.nieva@outlook.com>
2026-06-10 12:46:49 -0600

    Modelos de Autor y Carrera

--- models/usuario.py
+++ models/usuario.py
@@ -1,9 +1,10 @@
 class Usuario:
 
     # Constructor
-    def __init__(self, id_usuario, nombre, email, carrera):
+    def __init__(self, id_usuario, nombre, matricula, email, carrera):
         self.id_usuario = id_usuario
         self.nombre = nombre
+        self.matricula = matricula
         self.email = email
         self.carrera = carrera
         self.activo = True  # Por defecto, el usuario está activo
@@ -15,4 +16,4 @@ class Usuario:
         self.activo = False
 
     def mostrar_info(self):
-        return f"Usuario ID: {self.id_usuario}, Nombre: {self.nombre}, Email: {self.email}, Carrera: {self.carrera}, Activo: {'Sí' if self.activo else 'No'}"
+        return f"Usuario ID: {self.id_usuario}, Nombre: {self.nombre}, Matricula: {self.matricula}, Email: {self.email}, Carrera: {self.carrera}, Activo: {'Sí' if self.activo else 'No'}"
