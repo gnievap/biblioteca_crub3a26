52124544a55f3674ed4aafd5fab0c0e1bcb97f10
Gaby Nieva <gaby.nieva@outlook.com>
2026-06-16 10:26:47 -0600

    Funciones completas de LibroDAO

--- dao/libro_dao.py
+++ dao/libro_dao.py
@@ -1,6 +1,7 @@
 # DAO: Data Access Object
 # libro_dao: Objeto de acceso a datos de la tabla libro
 
+from database import conexion
 from database.conexion import Conexion
 from models.libro import Libro
 
@@ -26,3 +27,57 @@ class LibroDAO:
         cursor.close()
         conexion.close()
         return libros
+
+    def insertar(self, libro):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+        sql = """
+        INSERT INTO libro (titulo, autor, isbn, disponible)
+        VALUES (%s, %s, %s, %s)
+        """
+
+        cursor.execute(
+            sql,
+            (libro.titulo, 
+             libro.autor, 
+             libro.isbn, 
+             libro.disponible)
+        )
+
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+
+    def actualizar(self, libro):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+
+        sql = """
+        UPDATE libro
+        SET titulo = %s, autor = %s, isbn = %s, 
+        disponible = %s
+        WHERE id = %s
+        """
+        cursor.execute(
+            sql,
+            (libro.titulo, 
+             libro.autor, 
+             libro.isbn, 
+             libro.disponible,
+             libro.id)
+        )
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+
+    def eliminar(self, libro_id):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+
+        cursor.execute(
+            " DELETE FROM libro WHERE id = %s",
+            (libro_id,)
+            )
+        conexion.commit()
+        cursor.close()
+        conexion.close()
